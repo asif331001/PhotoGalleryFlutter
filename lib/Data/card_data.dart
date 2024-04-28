23f70a83fc47d643878697a8f2cddac77b35db65
asif331001 <asif95331001@gmail.com>
2024-04-28 15:28:40 +0600

    photo gallery

---
+++ lib/Data/card_data.dart
@@ -0,0 +1,60 @@
+class CardData {
+  static List<Map<String, String>> cards = [
+    {
+      "AlbumName": "Mood",
+      "AlbumPicture":
+          "https://images.pexels.com/photos/296282/pexels-photo-296282.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+      "AlbumHeader": "Mood",
+      "AlbumDescription":
+          "In our mood-themed photo gallery, every image is a window into the soul, capturing the essence of emotions in visual poetry. Join us on a journey through the evocative landscapes of mood, where each photograph speaks volumes in whispers of light, shadow, and the human experience."   },
+    {
+      "AlbumName": "Aesthetic",
+      "AlbumPicture":
+          "https://images.pexels.com/photos/6906621/pexels-photo-6906621.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+      "AlbumHeader": "Aesthetic",
+      "AlbumDescription":
+          "In our aesthetic-themed photo gallery, every image is a symphony of beauty and grace, where the subtle interplay of light and form ignites the senses. Join us in exploring the enchanting world of aesthetics, where each photograph whispers tales of elegance and inspiration."    },
+    {
+      "AlbumName": "Animals",
+      "AlbumPicture":
+          "https://images.pexels.com/photos/3608263/pexels-photo-3608263.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+      "AlbumHeader": "Animal",
+      "AlbumDescription":
+          "In the silent language of images, our photo gallery celebrates the enchanting world of animals. Each photograph is a portal into the mesmerizing realm of nature's creatures, capturing their beauty, grace, and untamed spirit. From the soaring flight of birds to the silent prowling of big cats, every frame tells a story of wonder and awe. Join us on a visual journey through the wilderness, where every click of the camera unveils a new chapter in the captivating saga of the animal kingdom."   },
+    {
+      "AlbumName": "City",
+      "AlbumPicture":
+          "https://images.pexels.com/photos/586687/pexels-photo-586687.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+      "AlbumHeader": "City",
+      "AlbumDescription":
+          "In the bustling heart of urban landscapes, our photo gallery captures the vibrant pulse of the city. Each image is a snapshot of life in motion, where skyscrapers stretch towards the sky and streets pulse with energy. From the neon glow of nightlife to the quiet moments of reflection in hidden alleyways, every photograph tells a story of diversity and dynamism. Join us as we explore the intricate tapestry of urban life, where the rhythm of the streets and the mosaic of cultures create an ever-evolving masterpiece."   },
+    {
+      "AlbumName": "Travel",
+      "AlbumPicture":
+          "https://images.pexels.com/photos/238622/pexels-photo-238622.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+      "AlbumHeader": "Travel",
+      "AlbumDescription":
+          "Embark on a visual odyssey with our travel-themed photo gallery, where every image is a window to the world. From the sun-kissed shores of tropical paradises to the snow-capped peaks of majestic mountains, each photograph invites you to wanderlust and explore. Discover the vibrant tapestry of cultures through bustling markets, ancient ruins, and architectural wonders that stand as testaments to human ingenuity. Whether traversing winding cobblestone streets in historic cities or traversing remote wilderness, every moment captured is a reminder of the boundless beauty and endless possibilities that await beyond our doorstep. Join us as we journey across continents and through time, capturing the essence of adventure and discovery in every frame."   },
+    {
+      "AlbumName": "Sky",
+      "AlbumPicture":
+          "https://images.pexels.com/photos/355465/pexels-photo-355465.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+      "AlbumHeader": "Sky",
+      "AlbumDescription":
+          "Step into the ethereal realm of the sky through our captivating photo gallery. Each image is a testament to the ever-changing canvas that stretches above us, painting the world with hues of blue, pink, and gold. From the fiery embrace of a sunset to the tranquil expanse of a starlit night, every photograph captures the boundless beauty and infinite wonder of the heavens. Witness the drama of thunderstorms brewing on the horizon, the delicate dance of clouds drifting lazily overhead, and the breathtaking spectacle of celestial bodies adorning the night sky. Join us on a journey through the atmospheric wonders that inspire awe and stir the imagination, where each frame invites you to soar beyond the confines of Earth and reach for the stars."    },
+    {
+      "AlbumName": "Road",
+      "AlbumPicture":
+          "https://images.pexels.com/photos/52531/way-clouds-seat-belts-direction-52531.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+      "AlbumHeader": "Road",
+      "AlbumDescription":
+          "In our road-themed photo gallery, each image is a passage to adventure and discovery, where every curve and bend beckons with the promise of new horizons and endless possibilities. Join us on a visual journey along the open road, where every mile traveled is a story waiting to be told."   },
+    {
+      "AlbumName": "Flower",
+      "AlbumPicture":
+          "https://images.pexels.com/photos/235683/pexels-photo-235683.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+      "AlbumHeader": "Flower",
+      "AlbumDescription":
+          "In our flower-themed photo gallery, each bloom tells a tale of beauty and resilience, where vibrant petals unfurl to reveal the poetry of nature's palette. Join us in celebrating the delicate elegance and timeless allure of floral wonders, where every photograph captures the essence of life's fleeting moments."   }
+  ];
+}
---
+++ lib/Screens/home.dart
@@ -0,0 +1,81 @@
+import 'package:assignment_module10/Screens/selected_album.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import '../Data/card_data.dart';
+import 'Widgets/appbar_back_icon.dart';
+import 'Widgets/appbar_title.dart';
+import 'Widgets/image_container.dart';
+import 'Widgets/pop_menu.dart';
+
+late int itemCount;
+late List<Map<String, String>> cards;
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  void initState() {
+    cards = CardData.cards;
+    itemCount = cards.length;
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          leading: Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: InkWell(
+              splashColor: Colors.transparent,
+              onTap: () {
+                SystemNavigator.pop();
+              },
+              child: backIcon(),
+            ),
+          ),
+          title: appbarTitle("Photo Gallery"),
+          centerTitle: true,
+          actions: [
+            popMenu(),
+          ],
+          backgroundColor: const Color(0xFF0074C6),
+        ),
+        body: SafeArea(
+          child: OrientationBuilder(
+            builder: (BuildContext context, Orientation orientation) {
+              return gridViewBuilder(orientation);
+            },
+          ),
+        ));
+  }
+
+  Widget gridViewBuilder(Orientation orientation) {
+    return GridView.builder(
+        padding: const EdgeInsets.all(15.00),
+        itemCount: itemCount,
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: (orientation == Orientation.portrait ? 2 : 4),
+          mainAxisSpacing: 30,
+          crossAxisSpacing: 35,
+          childAspectRatio: 1,
+        ),
+        itemBuilder: (context, index) {
+          return InkWell(
+            splashColor: Colors.transparent,
+            onTap: () {
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => SelectedAlbum(index: index)));
+            },
+            child: imageContainer(cards, index),
+          );
+        });
+  }
+}
